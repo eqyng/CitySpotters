3c5e4da8ffd5d8b876bc35d7b538ed0625a094d8
Elisa <thng3231@gmail.com>
2024-08-14 11:35:13 +1000

    Add new map

---
+++ LiveMap.js
@@ -0,0 +1,23 @@
+/*"use strict";
+
+function initMap() {
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+*/ 
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
